ef169615d118fb7a582c4a49a3efc425f27bdb04
matvey-27 <neutral_notes@tutamail.com>
2025-03-30 12:31:05 +0300

    update UserDB

--- DB/UserDB.cs
+++ DB/UserDB.cs
@@ -1,8 +1,18 @@
-// dotnet add package MongoDB.Driver
+using System.Threading.Tasks;
 using MongoDB.Driver;
 
 namespace DataBase;
 
 class UserDB{
-    
+    private static IMongoDatabase userdb = DataBaseDB.client.GetDatabase("user"); 
+
+    public static void CreateCollectionUser(){      
+        userdb.CreateCollection("users");
+    }
+    public static async Task<string> SingInUserAsync(string login , string password){
+
+
+
+        return "dd";
+    } 
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
-Console.WriteLine("hello world");
-Console.WriteLine(ServerInfo.ServerCryptor.AesEncryptionString("hello world"));
-Console.WriteLine(ServerInfo.ServerCryptor.AesDecryptionString(ServerInfo.ServerCryptor.AesEncryptionString("hello world")));
+using DataBase;
+
+// Console.WriteLine("hello world");
+// Console.WriteLine(ServerInfo.ServerCryptor.AesEncryptionString("hello world"));
+// Console.WriteLine(ServerInfo.ServerCryptor.AesDecryptionString(ServerInfo.ServerCryptor.AesEncryptionString("hello world")));
+
+UserDB.CreateCollectionUser();
