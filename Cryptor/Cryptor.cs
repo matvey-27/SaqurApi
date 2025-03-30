26bd215685ff51a5ccf88a3f7d40b085ee53c075
matvey-27 <neutral_notes@tutamail.com>
2025-03-30 03:15:38 +0300

    update cryptor

--- Cryptor/Cryptor.cs
+++ Cryptor/Cryptor.cs
@@ -1,5 +1,7 @@
 // https://learn.microsoft.com/ru-ru/dotnet/api/system.convert.tobase64string?view=net-8.0
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
+using System.Text;
 
 class Cryptor{
     public byte[] ServerRsaOpenKey = new byte[256];
@@ -12,6 +14,50 @@ class Cryptor{
         this.DataBaseAesKey = DataBaseAesKey;
         this.DataBaseAesIv = DataBaseAesIv;
     }
+    
+    // Нестатический метод для шифрования RSA
+    public string EncryptStringWithRsa(string plainText)
+    {
+        if (string.IsNullOrEmpty(plainText))
+            throw new ArgumentNullException(nameof(plainText));
+
+        using (RSA rsa = RSA.Create())
+        {
+            // Импортируем публичный ключ
+            rsa.ImportSubjectPublicKeyInfo(ServerRsaOpenKey, out _);
+            
+            // Шифруем данные
+            byte[] encryptedBytes = rsa.Encrypt(
+                Encoding.UTF8.GetBytes(plainText), 
+                RSAEncryptionPadding.OaepSHA256
+            );
+            
+            // Возвращаем Base64 строку
+            return Convert.ToBase64String(encryptedBytes);
+        }
+    }
+
+    public string DecryptStringWithRsa(string encryptedBase64)
+    {
+        if (string.IsNullOrEmpty(encryptedBase64))
+            throw new ArgumentNullException(nameof(encryptedBase64));
+
+        using (RSA rsa = RSA.Create())
+        {
+            // Импортируем приватный ключ
+            rsa.ImportRSAPrivateKey(ServerRsaPrivateKey, out _);
+            
+            // Декодируем Base64 и дешифруем
+            byte[] decryptedBytes = rsa.Decrypt(
+                Convert.FromBase64String(encryptedBase64), 
+                RSAEncryptionPadding.OaepSHA256
+            );
+            
+            // Возвращаем исходную строку
+            return Encoding.UTF8.GetString(decryptedBytes);
+        }
+    }
+
     public string AesEncryptionString(string data)
     {
         using Aes aes = Aes.Create();
